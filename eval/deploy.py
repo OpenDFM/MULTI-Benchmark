ae6d8fa9cfa877a0688e62da5b681deccba4004e
ethereal <m15635418665@163.com>
2024-02-23 00:09:00 +0800

    feat: chaneg file

--- eval/deploy.py
+++ eval/deploy.py
@@ -12,6 +12,7 @@ import zipfile
 
 # redirect stdout to string
 output = io.StringIO()
+err = io.StringIO()
 app = Flask(__name__)
 CORS(app)
 
@@ -22,13 +23,19 @@ args.label_file = "../data/problem_final.json"
 args.detail = True
 args.prediction_dir = "../results"
 source_suffix = "/source"
+paras_suffix = "/paras"
 prediction_file_suffix = "/prediction.json"
+prediction_file_name = "prediction.json"
+args_file_name = "args.json"
 paras_file_suffix = "/paras.json"
+source_zip_suffix = "/source.zip"
 result_zip_suffix = "/result.zip"
 
 
 def get_random_dir():
-    return os.path.join(args.prediction_dir, str(uuid.uuid4()))
+    random_dir = os.path.join(args.prediction_dir, str(uuid.uuid4()))
+    os.makedirs(random_dir)
+    return random_dir
 
 
 def save_prediction_json_to_target_dir(json_data, target_dir):
@@ -38,6 +45,7 @@ def save_prediction_json_to_target_dir(json_data, target_dir):
 
 
 def save_paras_to_target_dir(paras_data, target_dir):
+    os.makedirs(target_dir)
     with open(target_dir + paras_file_suffix, "w") as f:
         json.dump(paras_data, f)
 
@@ -55,31 +63,54 @@ def zip_dir(directory, zip_filename):
 
 @app.route("/generate", methods=["POST"])
 def generate():
-    prediction_json = request.json.get("prediction_json", None)
-    if prediction_json is None:
-        return jsonify({"result": "false", "data": None})
+    # get zip file from request
+    zip_file_base64 = request.json.get("zip_file", None)
+    if zip_file_base64 is None:
+        return jsonify({"result": "false", "data": None, "message": "zip_file is None"})
 
     # generate random dir
     random_dir = get_random_dir()
     source_dir = random_dir + source_suffix
+    paras_dir = random_dir + paras_suffix
 
-    # save prediction json to random dir
-    save_prediction_json_to_target_dir(json.loads(
-        prediction_json["fileContent"]), source_dir)
-    prediction_file = source_dir + prediction_file_suffix
+    # unzip zip file to source dir
+    try:
+        zip_file_blob = base64.b64decode(zip_file_base64)
+        with open(random_dir + source_zip_suffix, "wb") as f:
+            f.write(zip_file_blob)
+        with zipfile.ZipFile(random_dir + source_zip_suffix, 'r') as zip_ref:
+            zip_ref.extractall(source_dir)
+        os.remove(random_dir + source_zip_suffix)
+    except:
+        os.rmdir(random_dir)
+        return jsonify({"result": "false", "data": None, "message": "unzip error"})
 
-    # save request json too
+    # delete all files except prediction.json and args.json in source dir
+    for file in os.listdir(source_dir):
+        if file != prediction_file_name and file != args_file_name:
+            os.remove(os.path.join(source_dir, file))
+
+    # check if prediction.json exists
+    if not os.path.exists(source_dir + prediction_file_suffix):
+        os.rmdir(random_dir)
+        return jsonify({"result": "false", "data": None, "message": "prediction.json not exists"})
+
+    # save request json to paras dir
     paras_json = request.json
-    save_paras_to_target_dir(paras_json, source_dir)
+    save_paras_to_target_dir(paras_json, paras_dir)
 
     # change the args
     copy_args = copy.deepcopy(args)
-    copy_args.prediction_file = prediction_file
+    copy_args.prediction_file = source_dir + prediction_file_suffix
 
     # run the main function
     with redirect_stdout(output):
-        main(copy_args)
-    app.logger.info(output.getvalue())
+        try:
+            main(copy_args)
+        except Exception as e:
+            os.rmdir(random_dir)
+            return jsonify({"result": "false", "data": None, "message": str(e)})
+    # app.logger.info(output.getvalue())
 
     # zip the dir
     zip_dir(source_dir, random_dir + result_zip_suffix)
@@ -97,7 +128,8 @@ def generate():
             "other": {
                 "somthing": "else"
             }
-        }
+        },
+        "message": output.getvalue()
     }
 
     return jsonify(response_data)
