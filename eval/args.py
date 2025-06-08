15b7e77e503afd8685446821a87338b9ac0f3181
小臣子吃大橙子 <Jameszhuthethird@vip.qq.com>
2025-06-08 09:33:10 +0800

    update leaderboard

--- eval/args.py
+++ eval/args.py
@@ -38,7 +38,7 @@ model_list = {
         "split_sys": True,
     },
     "gemini-api": {
-        "avail_model": ["gemini-1.5-pro-latest","gemini-1.5-flash-latest"],
+        "avail_model": ["gemini-1.5-pro-latest","gemini-1.5-flash-latest","gemini-2.5-pro-preview-03-25","gemini-2.5-pro-preview-05-06","gemini-2.5-flash-preview-05-20","gemini-2.0-flash","gemini-2.0-flash-lite"],
         "model_type": "api",
         "support_input": [0, 1, 2, 3],
         "executor": "gpt",
